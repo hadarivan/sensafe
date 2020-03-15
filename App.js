2453a7c2bf190b84202022be91d38efc90b48664
hadarivan <38535949+hadarivan@users.noreply.github.com>
2020-03-15 11:05:02 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Simulation from '../Test/components/Simulation';
+
+export default class App extends Component {
+  render() {
+    return <Simulation />;
+  }
+}
