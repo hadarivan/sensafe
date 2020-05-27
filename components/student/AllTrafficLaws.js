5e96ca7147fd39286f57ecd0162d137726aed860
shaharrubinshtein <37486628+shaharrubinshtein@users.noreply.github.com>
2020-05-27 20:21:16 +0300

    All traffic laws - categories

--- components/student/AllTrafficLaws.js
+++ components/student/AllTrafficLaws.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import AnimatedLoader from 'react-native-animated-loader';
-import { FlatList,StyleSheet, View, Text} from 'react-native';
+import { FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
 
 export default class App extends Component {
 
   constructor() {
     super();
-    this.state = { 
-        allTrafficLaws: null,
-        loading: true,
-        gridView: true, 
-        HeaderText: 'חוקי תנועה' 
+    this.state = {
+      allTrafficLaws: null,
+      loading: true,
+      gridView: true,
+      HeaderText: 'חוקי תנועה',
+      subjectsList: null,
+      allTrafficLawsFiltered: false,
+      back: false
     }
   }
 
@@ -19,11 +22,32 @@ export default class App extends Component {
     fetch('https://sensafe-server.herokuapp.com/traffic_law')
       .then((response) => response.json())
       .then((responseJson) => {
-        this.setState({ allTrafficLaws: responseJson, loading: false });
+
+        // this extract unique subjects
+        let subjectsArray = [...responseJson]
+        // console.log(subjectsArray)
+        let uniqueSubjectsArray = subjectsArray.filter(function (elem, index, self) {
+          return index === self.findIndex(elm => elm.subject == elem.subject);
+        })
+
+        this.setState({ subjectsList: [...uniqueSubjectsArray], allTrafficLaws: [...responseJson], loading: false });
       })
       .catch((error) => {
         console.error(error);
       });
+  }
+  handleSubjectClick = (subject) => {
+    if (typeof subject != 'undefined') {
+      let allTrafficLawsFiltered = []
+      this.state.allTrafficLaws.forEach(element => {
+        if (element.subject == subject) {
+          allTrafficLawsFiltered.push(element)
+        }
+      })
+      this.setState({
+        allTrafficLawsFiltered: [...allTrafficLawsFiltered]
+      })
+    }
   }
 
   render() {
@@ -33,7 +57,7 @@ export default class App extends Component {
           (this.state.loading)
             ?
             (<View style={styles.loadingContainer}>
-               <AnimatedLoader
+              <AnimatedLoader
                 visible={true}
                 overlayColor="003f5c"
                 animationStyle={styles.lottie}
@@ -44,19 +68,34 @@ export default class App extends Component {
             :
             (<View style={{ flex: 1 }}>
               <View style={styles.Header}>
-                  <Text style={styles.HeaderText}>{this.state.HeaderText}</Text>
+                <Text style={styles.HeaderText}>{this.state.HeaderText}</Text>
               </View>
-            <View style ={styles.lawsHolder}>
-              <FlatList keyExtractor={(item) => item.law}
-                numColumns={(this.state.gridView) ? 1 : 0}
-                data={this.state.allTrafficLaws}
-                renderItem={({ item }) =>
-                  <View style ={styles.lawTextHolder}>
-                    <Text style={styles.lawText}>{item.law} </Text> 
-                  </View>
-                }/>
-            </View>
-        </View>)
+              <View style={styles.lawsHolder}>
+                {this.state.allTrafficLawsFiltered ?
+                  <FlatList keyExtractor={(item) => item.law}
+                    key={(this.state.gridView) ? 1 : 0}
+                    data={this.state.allTrafficLawsFiltered}
+                    renderItem={({ item }) =>
+                      <View style={styles.lawTextHolder}>
+                        <Text style={styles.lawText}>{item.law} </Text>
+                      </View>
+                } />
+                  :
+                  <FlatList keyExtractor={(item) => item.subject}
+                    numColumns={(this.state.gridView) ? 2 : 1}
+                    data={this.state.subjectsList}
+                    renderItem={({ item }) =>
+                      <TouchableOpacity
+                        onPress={this.handleSubjectClick.bind(this, item.subject)}>
+                        <View style={styles.SubjectTextHolder}>
+                          <Text style={styles.SubjectText}>{item.subject} </Text>
+                        </View>
+                      </TouchableOpacity>
+                    } 
+                  />
+                }
+              </View>
+            </View>)
         }
       </View>
     );
@@ -66,15 +105,15 @@ export default class App extends Component {
 const styles = StyleSheet.create(
   {
     container: {
-      flex:1,
-      backgroundColor:'#d3edea'
+      flex: 1,
+      backgroundColor: '#d3edea'
     },
     lawsHolder: {
       margin: 5,
       height: 30,
       flex: 1,
       position: 'relative'
-      
+
     },
     lawTextHolder: {
       position: 'relative',
@@ -84,20 +123,50 @@ const styles = StyleSheet.create(
       backgroundColor: 'rgba(255,255,255,1)',
       paddingHorizontal: 10,
       paddingVertical: 13,
-      marginBottom:5,
-      borderRadius:5,
+      marginBottom: 5,
+      borderRadius: 5,
       shadowColor: "#000",
       shadowOffset: {
-          width: 0,
-          height: 1,
+        width: 0,
+        height: 1,
       },
       shadowOpacity: 0.10,
       shadowRadius: 2.5,
-  
+
       elevation: 2,
     },
+    SubjectTextHolder: {
+      position: 'relative',
+      left: 0,
+      bottom: 0,
+      right: 0,
+      backgroundColor: 'rgba(255,255,255,1)',
+      marginHorizontal: 3,
+      height:165,
+      width:170,
+      paddingVertical: 13,
+      marginBottom: 5,
+      borderRadius: 5,
+      backgroundColor: '#93d1bc',
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.10,
+      shadowRadius: 2.5,
+
+      elevation: 2,
+    },
+
     lawText: {
       color: 'black'
+    },
+    SubjectText: {
+      color: 'white',
+      fontWeight:"bold",
+      textAlign:"center",
+      marginTop:62
     },
     lottie: {
       width: 100,
@@ -111,5 +180,11 @@ const styles = StyleSheet.create(
       color: 'white',
       textAlign: 'center',
       alignSelf: 'stretch'
+    },
+    back: {
+      width: 30,
+      height: 30,
+      position: 'absolute',
+      bottom: 100,
     }
   });
