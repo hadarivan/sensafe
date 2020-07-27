ddc326d639a21a649057edef0a7aa0d46f8abbfc
shaharrubinshtein <37486628+shaharrubinshtein@users.noreply.github.com>
2020-07-27 16:52:35 +0300

    update

--- components/student/TrafficSignImage.js
+++ components/student/TrafficSignImage.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
-import {Image, StyleSheet, View } from 'react-native';
+/* eslint-disable react/prop-types */
+import React, { Component } from 'react'
+import { Image, StyleSheet, View } from 'react-native'
 
 export default class TrafficSignImage extends Component {
-
-  constructor() {
-    super();
+  // eslint-disable-next-line no-useless-constructor
+  constructor () {
+    super()
   }
-  
-  render() {
+
+  // traffic sign image declaration for grid view
+  render () {
     return (
       <View style={styles.imageHolder}>
         <Image source={{ uri: this.props.imageURI }} style={styles.image} />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create(
-{ 
-  imageHolder: {
-    marginTop: 4,
-    marginRight:4,
-    marginLeft:4,
-    height: 175,
-    flex: 1,
-    position: 'relative'
-  },
-  image: {
+  {
+    imageHolder: {
+      marginTop: 4,
+      marginRight: 4,
+      marginLeft: 4,
+      height: 175,
+      flex: 1,
+      position: 'relative'
+    },
+    image: {
       height: '100%',
       width: '100%',
-      borderTopRightRadius:5,
-      borderTopLeftRadius:5
-      
+      borderTopRightRadius: 5,
+      borderTopLeftRadius: 5
+
     }
-});
+  })
