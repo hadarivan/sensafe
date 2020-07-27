36b2f82768e0c68fe98761a4f476d55030e8c026
shaharrubinshtein <37486628+shaharrubinshtein@users.noreply.github.com>
2020-07-27 16:43:13 +0300

    traffic signs update

--- components/student/AllTrafficSigns.js
+++ components/student/AllTrafficSigns.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import AnimatedLoader from 'react-native-animated-loader';
-import {  Alert, Modal, FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+/* eslint-disable eqeqeq */
+import React, { Component } from 'react'
+import AnimatedLoader from 'react-native-animated-loader'
+import { Alert, Modal, FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import TrafficSignImage from './TrafficSignImage'
 
-
 export default class AllTrafficSigns extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
+  constructor (props) {
+    super(props)
+    this.state = {
       allTrafficSigns: null,
       loading: true,
       gridView: true,
       HeaderText: 'תמרורים',
-      isVisible:false,
-      selectedId:false
+      isVisible: false,
+      selectedId: false
     }
   }
 
-  componentDidMount() {
+  // fetching all traffic signs from DataBase
+  componentDidMount () {
     fetch('https://sensafe-server.herokuapp.com/traffic_sign')
       .then((response) => response.json())
       .then((responseJson) => {
-        this.setState({ allTrafficSigns: responseJson, loading: false });
+        this.setState({ allTrafficSigns: responseJson, loading: false })
       })
       .catch((error) => {
-        console.error(error);
-      });
+        console.error(error)
+      })
   }
-   // hide show modal
-   displayModal(show,id){
-    this.setState({isVisible: show,selectedId:id})
+
+  // show modal - open specific traffic sign
+  displayModal (show, id) {
+    this.setState({ isVisible: show, selectedId: id })
   }
-  render() {
-   
-    var trafficSigns = this.state.allTrafficSigns;
-    var selectedObject=this.state.selectedId?trafficSigns.find(item=>item.id==this.state.selectedId):false
-    console.log(selectedObject)
+
+  render () {
+    const trafficSigns = this.state.allTrafficSigns
+    const selectedObject = this.state.selectedId ? trafficSigns.find((item) => item.id == this.state.selectedId) : false
     return (
       <View style={styles.container} >
         {
           (this.state.loading)
-            ?
-            (<View style={styles.lottie}>
+            ? (<View style={styles.lottie}>
               <AnimatedLoader
                 visible={true}
                 overlayColor="003f5c"
@@ -50,69 +50,69 @@ export default class AllTrafficSigns extends Component {
                 source={require('../student/images/18535-best-bike-guide-bicycle.json')}
               />
             </View>)
-            :
-            (<View style={{ flex: 1}}>
-                <View style={styles.Header}>
+            : (<View style={{ flex: 1 }}>
+              <View style={styles.Header}>
                 <Text style={styles.HeaderText}>{this.state.HeaderText}</Text>
-                </View>
-             <Modal 
-                animationType = {"slide"}
+              </View>
+              <Modal
+                animationType = {'slide'}
                 transparent={false}
                 visible={this.state.isVisible}
                 onRequestClose={() => {
-                  Alert.alert('Modal has now been closed.');
+                  Alert.alert('Modal has now been closed.')
                 }}>
-               
-              <View style={{flex:1}}>
-                <View style = {styles.textViewHolderOnPress}>
-                  <Text style={styles.textOnImageOnPress}>{selectedObject.name}</Text>
-                </View>
-                <View style={styles.imageHolderOnPress}>
-                  <TrafficSignImage imageURI={selectedObject.image} style={styles.imageOnPress} />
-                  <View style={styles.toDoContainer}>
-                    <Text style={styles.meaning}>משמעות התמרור</Text>
-                    <Text style={styles.toDo}>{selectedObject.toDo}</Text> 
-                  </View> 
-                </View>
-              </View>
 
-              <Text 
-                style={styles.closeText}
-                onPress={() => {
-                  this.displayModal(!this.state.isVisible);}}>סגור</Text>
+                <View style={{ flex: 1 }}>
+                  <View style = {styles.textViewHolderOnPress}>
+                    <Text style={styles.textOnImageOnPress}>{selectedObject.name}</Text>
+                  </View>
+                  <View style={styles.imageHolderOnPress}>
+                    <TrafficSignImage imageURI={selectedObject.image} style={styles.imageOnPress} />
+                    <View style={styles.toDoContainer}>
+                      <Text style={styles.meaning}>משמעות התמרור</Text>
+                      <Text style={styles.toDo}>{selectedObject.toDo}</Text>
+                    </View>
+                  </View>
+                </View>
+
+                <Text
+                  style={styles.closeText}
+                  onPress={() => {
+                    this.displayModal(!this.state.isVisible)
+                  }}>סגור</Text>
               </Modal>
-              
+
               <FlatList keyExtractor={(item) => item.id}
-                  numColumns={this.state.gridView ? 2 : 1}
-                  data={trafficSigns}
-                  renderItem={({ item }) =>
-                    <TouchableOpacity onPress={() => {
-                      this.displayModal(true,item.id);
-                      }} style={{flex:1}}>
-                      <TrafficSignImage imageURI={item.image}/>
-                      <View style={styles.textViewHolder}>
-                        <Text numberOfLines={1} style={styles.textOnImage}>{item.name}</Text>
-                      </View>
-                    </TouchableOpacity> 
-                  }/>
+                numColumns={this.state.gridView ? 2 : 1}
+                data={trafficSigns}
+                renderItem={({ item }) =>
+                  <TouchableOpacity onPress={() => {
+                    this.displayModal(true, item.id)
+                  }} style={{ flex: 1 }}>
+                    <TrafficSignImage imageURI={item.image}/>
+                    <View style={styles.textViewHolder}>
+                      <Text numberOfLines={1} style={styles.textOnImage}>{item.name}</Text>
+                    </View>
+                  </TouchableOpacity>
+                }/>
             </View>)
         }
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create(
-  {  
-  container: {
-    flex: 1,
-    backgroundColor:'#b5e2ff'
+  {
+    container: {
+      flex: 1,
+      backgroundColor: '#b5e2ff'
 
-  },
-  lottie: {
-    width: 100, 
-    height: 100
-  },
+    },
+    lottie: {
+      width: 100,
+      height: 100
+    },
     Header: {
       padding: 15,
       backgroundColor: 'rgba(77,170,238,0.75)'
@@ -125,14 +125,14 @@ const styles = StyleSheet.create(
     textViewHolderOnPress: {
       position: 'relative',
       left: 0,
-      top:0,
+      top: 0,
       bottom: 0,
       right: 0,
-      height:50,
+      height: 50,
       backgroundColor: 'rgba(77,170,238,0.75)',
       paddingHorizontal: 10,
       paddingVertical: 9,
-      alignItems: 'center',
+      alignItems: 'center'
     },
     textOnImageOnPress: {
       color: 'white',
@@ -141,52 +141,51 @@ const styles = StyleSheet.create(
     },
     imageHolderOnPress: {
       margin: 5,
-      marginBottom:35,
+      marginBottom: 35,
       flex: 1,
       position: 'relative',
       justifyContent: 'center'
     },
     imageOnPress: {
-        padding:25,
-        alignItems: 'center'
+      padding: 25,
+      alignItems: 'center'
     },
-    toDoContainer:{
-        paddingTop:25
-        
+    toDoContainer: {
+      paddingTop: 25
+
     },
-    meaning:{
-      paddingRight:15,
-      fontSize:18,
-      fontWeight:'bold',
-      color:'#000000'
+    meaning: {
+      paddingRight: 15,
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: '#000000'
     },
     toDo: {
       fontSize: 18,
-      paddingLeft:15,
-      paddingRight:15,
-      paddingBottom:30
+      paddingLeft: 15,
+      paddingRight: 15,
+      paddingBottom: 30
     },
     closeText: {
       fontSize: 26,
       color: '#00479e',
-      textAlign: 'center',
+      textAlign: 'center'
     },
     textViewHolder: {
       position: 'relative',
       left: 0,
-      top:0,
+      top: 0,
       bottom: 0,
       right: 0,
       backgroundColor: 'rgba(77,170,238,0.75)',
       marginHorizontal: 4,
       paddingVertical: 9,
-      borderBottomLeftRadius:5,
-      borderBottomRightRadius:5,
+      borderBottomLeftRadius: 5,
+      borderBottomRightRadius: 5,
       alignItems: 'center',
-      marginBottom:5
+      marginBottom: 5
     },
     textOnImage: {
       color: 'white'
     }
-  });
- 
+  })
